67c7e5d55a5fdea4fbeb9b4230fb0b836866c22b
Nikita Nafranets <n.nafranets@vk.team>
2023-02-11 16:05:29 +0300

    Initial Commit

---
+++ YubiKey Tap Notifier/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YubiKey Tap Notifier
+//
+//  Created by n.nafranets on 11.02.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
